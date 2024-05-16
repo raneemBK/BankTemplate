f7be29fe2200714071a2cd748a6e866d8849eece
Raneem Bakkar <121720207+raneemBK@users.noreply.github.com>
2024-05-16 14:32:38 +0300

    version 1.5

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,26 +12,55 @@ menuRes.addEventListener("click",()=>{
     menu_disabl.classList.add("menu-disabl")
 })
 
-// Get the button
-let mybutton = document.getElementById("myBtn");
+// // Get the button
+// let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// // When the user scrolls down 20px from the top of the document, show the button
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "block";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+// // When the user clicks on the button, scroll to the top of the document
+// function topFunction() {
+//   document.body.scrollTop = 0;
+//   document.documentElement.scrollTop = 0;
+// }
 
 
 // transform testimonials
 
+// var next_testimonial = document.getElementById("next-testimonial");
+// var prev_testimonial = document.getElementById("prev-testimonial");
+// var sildes = document.getElementById("sildes");
+// next_testimonial.addEventListener("click",()=>{
+//   debugger;
+//   sildes.style.transform = "translateX(0px)";
+// })
+
+var nextTestimonial = document.getElementById("next-testimonial");
+var prevTestimonial = document.getElementById("prev-testimonial");
+var slides = document.querySelector(".testimonial-container-cards");
+var slideIndex = 0;
+var slideWidth = slides.offsetWidth; // Adjust this according to your card width
+
+nextTestimonial.addEventListener("click", () => {
+    slideIndex++;
+    if (slideIndex * slideWidth <= slides.scrollWidth) {
+        slides.style.transform = "translateX(" + -slideIndex * slideWidth + "px)";
+    } else {
+        slideIndex--;
+    }
+});
+
+prevTestimonial.addEventListener("click", () => {
+    if (slideIndex > 0) {
+        slideIndex--;
+        slides.style.transform = "translateX(" + -slideIndex * slideWidth + "px)";
+    }
+});
