6f67e6a7cde944ef2d717e6b7b14b72f0d77c2e4
Raneem Bakkar <121720207+raneemBK@users.noreply.github.com>
2024-05-28 12:49:25 +0300

    version 2.2

--- assets/js/index.js
+++ assets/js/index.js
@@ -98,3 +98,17 @@ window.addEventListener("resize", () => {
     var slideWidth = getSlideWidth();
     slides.style.transform = "translateX(" + -slideIndex * slideWidth + "px)";
 });
+
+// login and register page
+const wrapper = document.querySelector(".wrapper");
+const signupHeader = document.querySelector(".signup header");
+const loginpHeader = document.querySelector(".login header");
+
+loginpHeader.addEventListener("click",()=>{
+    wrapper.classList.add("active");
+
+})
+signupHeader.addEventListener("click",()=>{
+    wrapper.classList.remove("active");
+    
+})
